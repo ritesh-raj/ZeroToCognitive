bc4643dc107e7718a6825c1bd1e3187681c0e5a7
Ritesh Raj <ritesh.raj@in.ibm.com>
2018-06-08 11:34:14 +0530

    Code updated
    Updates

--- Chapter03/controller/restapi/features/speech_to_text.js
+++ Chapter03/controller/restapi/features/speech_to_text.js
@@ -4,5 +4,24 @@ var vcapServices = require('vcap_services');
 var config = require('../../env.json');
 
 exports.token = function(req, res) {
+    // the extend function adds additional information into our credentials from within the 
+    // Watson and Bluemix operating environment
+    var sttConfig = extend(config.speech_to_text, vcapServices.getCredentials('speech_to_text'));
+    // request authorization to access the service
+    var sttAuthService = watson.authorization(sttConfig);
 
+    // now that we're authenticated, get the token
+    sttAuthService.getToken({
+        url: sttConfig.url
+    }, function(err, token) {
+        if (err) {
+            // send an error back if we cannot retrieve the token successfully
+            console.log('Error retrieving token: ', err);
+            res.status(500).send('Error retrieving token'+ReferenceError);
+            return;
+        }
+        // if we're successful, then send the new token back to the browser
+        res.send(token);
+    });
 }
+
